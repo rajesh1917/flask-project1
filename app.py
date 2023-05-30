0b66d9476a7206dc2ef495241fba47e44dfe085a
RAJESH S <rajeshnaidu1957@gmail.com>
2023-05-30 13:13:34 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask ,render_template
+app=Flask(__name__) 
+
+@app.route("/login")
+def first():
+    return render_template("login.html") 
+
+if __name__ == "__main__":
+    app.run(debug="True")
